aee0d571f78d010f6d9d423c7a2460e2702e1e20
Mantri Jaya Pravallika <mantrijayapravallika05@gmail.com>
2025-11-23 12:06:53 +0530

    Update wsgi.py

--- Surprise/wsgi.py
+++ Surprise/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Suprise project.
+WSGI config for Surprise project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Suprise.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Surprise.settings')
 
 application = get_wsgi_application()
